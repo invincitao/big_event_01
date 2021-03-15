c57de73300ac019d2539365b1b9b41042bb15375
dxm <dxm@123.cn>
2021-03-15 09:58:43 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,7 +7,28 @@ let baseURL = "http://api-breakingnews-web.itheima.net";
 
 // 拦截ajax请求
 $.ajaxPrefilter(function (options) {
+
+    // 1.如果是index.html页面，不需要添加前缀
+    if (options.url === 'http://127.0.0.1:5500/index.html') {
+        return;
+    }
     // console.log(options.url);
     // 拼接对应服务器环境
     options.url = baseURL + options.url;
+    // 2.身份认证,每个带my的接口都要用到，所以就简化一下代码
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('mytoken') || ''
+        }
+    };
+    // 登录拦截(拦截所有登录，判断身份认证信息)
+    options.complete = function (res) {
+        console.log(res);
+        let obj = res.responseJSON;
+        // 认证失败就清空token，跳转到登录页
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            localStorage.removeItem('mytoken');
+            location.href = '/login.html';
+        }
+    };
 })
---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+// 入口函数
+$(function () {
+
+    // 1.获取用户信息
+    getuserInfo();
+    // 2.退出登录,回到登陆页面，清除token
+    $('#btnLoginout').on('click', function () {
+        layer.confirm('是否确认退出？', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('mytoken');
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+});
+// 封装需要放到入口函数外边
+// 原因:后面其他的页面要用
+function getuserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        type: 'GET',
+        // ---------------------
+        // headers: {
+        //     Authorization: localStorage.getItem('mytoken') || ''
+        // },
+        // 登录拦截
+        // complete: function (res) {
+        //     console.log(res);
+        //     let obj = res.responseJSON;
+        //     if (obj.status == 1 && obj.message == '身份认证失败！') {
+        //         localStorage.removeItem('mytoken');
+        //         location.href = '/login.html';
+        //     }
+        // },
+        // --------------------------
+        // 上面的代码写在了baseAPI.js里边
+        data: {},
+        success: (res) => {
+            if (res.status != 0) {
+                return layui.layer.msg(res.message, { icon: 5 })
+            }
+            renderAvatar(res.data);
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // console.log(user);
+    // 1.获取名字,进行渲染
+    let name = user.nickname || user.username;
+    $('#welcome').html('欢迎光临&nbsp;' + name);
+    if (user.user_pic != null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide();
+    } else {
+        // 没有头像
+        $('.layui-nav-img').hide();
+        let text = name[0].toUpperCase();
+        $('.text-avatar').show().html(text)
+    }
+}
+
+
